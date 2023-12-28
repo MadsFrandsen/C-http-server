7ea2a76b0c42a0bf1711e36480a3314904692f41
Mads Frandsen <madschristianfrandsen@gmail.com>
2023-12-28 19:41:40 +0100

    get file implemented

---
+++ get_file.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char *get_file(char *fileName) {
+    // open file
+    FILE *fp = fopen(fileName, "r");
+
+    if (fp == NULL) {
+        printf("File %s does not exist\n", fileName);
+        return NULL;
+    }
+
+    // set file pointer at end of file
+    if (fseek(fp, 0, SEEK_END) != 0) {
+        printf("fseek failed.\n");
+        return NULL;
+    }
+
+    // get file size
+    int fileSize = ftell(fp);
+
+    if (fileSize == -1) {
+        printf("ftell failed.\n");
+        return NULL;
+    }
+
+    // set file pointer at start of file
+    if (fseek(fp, 0, SEEK_SET) != 0) {
+        printf("fseek failed.\n");
+        return NULL;
+    }
+
+    // malloc buffer
+    char *buffer = (char *)malloc(fileSize + 1);
+
+    // read contents of file to buffer
+    size_t bytesRead = fread(buffer, fileSize, 1, fp);
+
+    if (bytesRead != (size_t)fileSize) {
+        printf("fread failed.\n");
+        fclose(fp);
+        free(buffer);
+        return NULL;
+    }
+
+    // null terminate buffer and close file
+    buffer[fileSize] = '\0';
+    fclose(fp);
+
+    // return buffer
+    return buffer;
+}
--- server.c
+++ server.c
@@ -14,21 +14,25 @@ void init_server(Server *server, int port) {
     int server_socket;
     struct sockaddr_in address;
 
+    // create the socket
     if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket failed");
         exit(EXIT_FAILURE);
     }
 
+    // clear data stored &adress and set values
     bzero(&address, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
+    // bind the socket to the address
     if (bind(server_socket, (struct sockaddr*)&address, sizeof(address)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
+    // set socket to listen for connections
     if (listen(server_socket, 5) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
